7f3f297000420980d3519c8977f763a571c0534a
ITMEVN <buituanv@gmail.com>
2018-10-12 03:04:07 +0700

    rfgyerg

---
+++ public/precache-manifest.299cf04a5beabda47e8e30ac82e22863.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdd6fc95af00e9ef9ac5",
+    "url": "/static/js/main.cdd6fc95.chunk.js"
+  },
+  {
+    "revision": "637d6af399803a2c7b7d",
+    "url": "/static/js/1.637d6af3.chunk.js"
+  },
+  {
+    "revision": "cdd6fc95af00e9ef9ac5",
+    "url": "/static/css/main.40034ca0.chunk.css"
+  },
+  {
+    "revision": "637d6af399803a2c7b7d",
+    "url": "/static/css/1.8ebddf25.chunk.css"
+  },
+  {
+    "revision": "5098e084b9e746f530672098aef6b353",
+    "url": "/index.html"
+  }
+];
